b363ed1320ce6dcd003594786747894b9c7b4773
diniali0211 <105869721+diniali0211@users.noreply.github.com>
2026-05-19 10:31:30 +0800

    Update connection.py

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -8,16 +8,11 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///.ats.db")
 
 if DATABASE_URL.startswith("postgres://"):
-    DATABAESE_URL = DATBASE_URL.replace("postgres://", "postgresql+asyncpg://", 1)
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://", 1)
 
 engine = create_async_engine(
     DATABASE_URL,
     echo=True,
-)
-
-engine = create_async_engine(
-  DATABASE_URL,
-  echo=True,
 )
 
 SessionLocal = sessionmaker(
@@ -29,5 +24,5 @@ SessionLocal = sessionmaker(
 Base = declarative_base()
 
 async def get_db():
-  async with SessionLocal() as session:
-      yield session
+    async with SessionLocal() as session:
+        yield session
